d78f7a687aedb54fe4d6aac81a57146e19df7e25
malachi-user <malachigutman@gmail.com>
2026-01-15 11:33:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/nix/store/chromium/bin/chromium',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ]
+    }
 });
-
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
     console.log('QR Code received, scan with WhatsApp');
